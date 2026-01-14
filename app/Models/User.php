0638ce8571a21deea9deb6d29c1c6dba3724847e
Mohammad <m@fr33.tv>
2026-01-14 14:22:54 +0530

    Working login

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,17 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Osiset\ShopifyApp\Contracts\ShopModel as IShopModel;
+use Osiset\ShopifyApp\Traits\ShopModel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements IShopModel
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, SoftDeletes, ShopModel;
 
     /**
      * The attributes that are mass assignable.
@@ -42,7 +45,7 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            //'password' => 'hashed',
         ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $shop = auth()->user();
+    $productsResponse = $shop->api()->rest('GET', '/admin/api/2026-01/products.json');
+    logger()->info('Products: ' . json_encode($productsResponse));
+    $products = $productsResponse['body']['products'] ?? [];
+    $error = $productsResponse['errors'] ? $productsResponse['body']: '';
+    return view('welcome', compact('products', 'error'));
+})->middleware(['verify.shopify'])->name('home');
