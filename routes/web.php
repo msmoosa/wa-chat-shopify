4c331560bd6b4a81bdf0857bd501493d2dd0145d
Mohammad <m@fr33.tv>
2026-01-29 12:28:36 +0530

    Some minor reformatting

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,9 @@ use App\Http\Controllers\ManualTemplateController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])
-    ->middleware(['verify.shopify'])->name('home');
+    ->middleware(['verify.shopify'])
+    ->name('home');
+
 // Test page route
 Route::get('/test/page', [TestPageController::class, 'index'])
     ->name('test.page');
